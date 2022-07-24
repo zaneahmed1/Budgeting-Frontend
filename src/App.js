88af11af34b204b1ca160942942b9b9d853d656e
Zane <zaneahmed4@gmail.com>
2022-07-24 12:00:23 -0400

    start

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import NavBar from "./Components/NavBar";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/transactions" element={<Index />} />
+            <Route path="/transactions/new" element={<New />} />
+            <Route path="/transactions/:index" element={<Show />} />
+            <Route path="/transactions/:index/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h2>Welcome to The Budgeting App</h2>
+    </div>
+  )
+}
